f04158ca95f0b93a9ac7a473849a8f189722740a
Ivan Misarosh <ivan.misarosh05@gmail.com>
2025-04-22 23:20:35 +0300

    Added visit search bar and filter form

--- carservice/service_site/filters.py
+++ carservice/service_site/filters.py
@@ -1,6 +1,8 @@
 import django_filters
 from django import forms
-from .models import Customer
+from .models import Customer, Visit
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
 
 class CustomerFilter(django_filters.FilterSet):
     first_name = django_filters.CharFilter(
@@ -30,3 +32,112 @@ class CustomerFilter(django_filters.FilterSet):
     class Meta:
         model = Customer
         fields = ["first_name", "last_name", "email", "phone_number"]
+
+
+class VisitFilter(django_filters.FilterSet):
+    visit_date__gte = django_filters.DateFilter(
+        field_name='visit_date', lookup_expr='gte', label='Visit Date From',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    visit_date__lte = django_filters.DateFilter(
+        field_name='visit_date', lookup_expr='lte', label='Visit Date To',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    planned_end_date__gte = django_filters.DateFilter(
+        field_name='planned_end_date', lookup_expr='gte', label='Planned End From',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    planned_end_date__lte = django_filters.DateFilter(
+        field_name='planned_end_date', lookup_expr='lte', label='Planned End To',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    actual_end_date__gte = django_filters.DateFilter(
+        field_name='actual_end_date', lookup_expr='gte', label='Actual End From',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    actual_end_date__lte = django_filters.DateFilter(
+        field_name='actual_end_date', lookup_expr='lte', label='Actual End To',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    payment_date__gte = django_filters.DateFilter(
+        field_name='payment_date', lookup_expr='gte', label='Payment Date From',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    payment_date__lte = django_filters.DateFilter(
+        field_name='payment_date', lookup_expr='lte', label='Payment Date To',
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+
+    visit_status = django_filters.ModelChoiceFilter(
+        queryset=Visit.visit_status.field.related_model.objects.all(),
+        label='Visit Status',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    car = django_filters.ModelChoiceFilter(
+        queryset=Visit.car.field.related_model.objects.select_related('car_model__car_brand', 'customer'),
+        label='Car',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    employee = django_filters.ModelChoiceFilter(
+        queryset=Visit.employee.field.related_model.objects.all(),
+        label='Employee',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    payment_status = django_filters.ModelChoiceFilter(
+        queryset=Visit.payment_status.field.related_model.objects.all(),
+        label='Payment Status',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    visit_number = django_filters.NumberFilter(
+        label='Visit Number',
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    price__gte = django_filters.NumberFilter(
+        field_name='price', lookup_expr='gte', label='Min Price',
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    price__lte = django_filters.NumberFilter(
+        field_name='price', lookup_expr='lte', label='Max Price',
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Visit
+        fields = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.form.helper = FormHelper()
+        self.form.helper.form_method = 'get'
+        self.form.helper.form_id = 'visit-filter-form'
+        self.form.helper.layout = Layout(
+            Row(
+                Column('visit_date__gte', css_class='form-group col-md-6 mb-0'),
+                Column('visit_date__lte', css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column('planned_end_date__gte', css_class='form-group col-md-6 mb-0'),
+                Column('planned_end_date__lte', css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column('actual_end_date__gte', css_class='form-group col-md-6 mb-0'),
+                Column('actual_end_date__lte', css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column('payment_date__gte', css_class='form-group col-md-6 mb-0'),
+                Column('payment_date__lte', css_class='form-group col-md-6 mb-0')
+            ),
+            Row(
+                Column('visit_status', css_class='form-group col-md-6 mb-0'),
+                Column('payment_status', css_class='form-group col-md-6 mb-0')
+            ),
+            'car',
+            'employee',   
+            'visit_number',
+            Row(
+                Column('price__gte', css_class='form-group col-md-6 mb-0'),
+                Column('price__lte', css_class='form-group col-md-6 mb-0')
+            ),
+            Submit('submit', 'Search', css_class='btn btn-primary')
+        )
--- carservice/service_site/views/visit_views.py
+++ carservice/service_site/views/visit_views.py
@@ -3,6 +3,8 @@ from django.core.paginator import Paginator
 from .. import models
 from django.views.generic import View 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from ..filters import VisitFilter
+from django.db.models import Q
 
 
 class Visits(LoginRequiredMixin, PermissionRequiredMixin, View):
@@ -12,16 +14,35 @@ class Visits(LoginRequiredMixin, PermissionRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         page_number = request.GET.get("page", 1)
         items_per_page = int(request.GET.get("items_per_page", 10))
+        search_query = request.GET.get("search", '')
+
+        search_terms = search_query.split()
+        query = Q()
+
+        for term in search_terms:
+            query |= Q(car__car_model__car_brand__brand_name__icontains=term) | Q(
+                car__car_model__model_name__icontains=term) | Q(
+                car__customer__first_name__icontains=term) | Q(
+                car__customer__last_name__icontains=term) | Q(
+                visit_number__icontains=term) | Q(
+                employee__first_name__icontains=term) | Q(
+                employee__last_name__icontains=term) | Q(
+                visit_status__status_name__icontains=term) | Q(
+                payment_status__payment_name__icontains=term) | Q(
+                car__customer__phone_number__icontains=term)
 
         visits = models.Visit.objects.all().select_related(
-            'visit_status', 'car', 'car__customer', 'car__car_model', 'employee', 'payment_status').order_by('-visit_date')
+            'visit_status', 'car', 'car__customer', 'car__car_model', 'employee', 'payment_status').filter(query).order_by('-visit_date')
 
+        visit_filter = VisitFilter(request.GET, queryset=visits)
+        visits_qs = visit_filter.qs
 
-        paginator = Paginator(visits, items_per_page)
+        paginator = Paginator(visits_qs, items_per_page)
         page_obj = paginator.get_page(page_number)
 
         context = {
             "visits": page_obj,
+            "filter_form": visit_filter.form,
         }
 
         return render_htmx(request, "service_site/visits/visits.html", "service_site/visits/_visit_list.html", context)
